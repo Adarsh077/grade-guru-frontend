fc0b9058655cdfac4e0a180493bde3afb7c9de9e
KiraKami <akshatkhair41@gmail.com>
2024-03-15 21:29:10 +0530

    feat(addMasterSemester) : toggle group

--- src/features/MasterList/Semester/AddSemester/AddMasterSemesterForm.jsx
+++ src/features/MasterList/Semester/AddSemester/AddMasterSemesterForm.jsx
@@ -1,32 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
-import * as z from "zod";
 
 import { Button } from "@/components/ui/button";
 import { DialogFooter } from "@/components/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  FormRootError,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { addMasterSemesters } from "@/store/master-list/semester/semester.actions";
 import {
   addMasterSemesterErrorSelector,
   isCallingAddMasterSemesterApiSelector,
 } from "@/store/master-list/semester/semester.selectors";
-
-const addMasterSemesterSchema = z.object({
-  name: z.string().min(1, "Name is required!"),
-  number: z.string().min(1, "Number is required!"),
-});
 
 const AddMasterSemesterForm = ({ departmentId, handleClose }) => {
   const dispatch = useDispatch();
@@ -34,39 +17,27 @@ const AddMasterSemesterForm = ({ departmentId, handleClose }) => {
   const isCallingAddMasterSemesterApi = useSelector(
     isCallingAddMasterSemesterApiSelector
   );
+
   const addMasterSemesterError = useSelector(addMasterSemesterErrorSelector);
 
-  const form = useForm({
-    resolver: zodResolver(addMasterSemesterSchema),
-    defaultValues: {
-      name: "",
-      number: "1",
-    },
-  });
+  const [selectedSemester, setSelectedSemester] = useState("3");
 
   useEffect(() => {
-    if (!addMasterSemesterError) return;
-    if (addMasterSemesterError.errors) {
-      if (addMasterSemesterError.errors.name) {
-        form.setError("name", { message: addMasterSemesterError.errors.name });
-      }
-      if (addMasterSemesterError.errors.number) {
-        form.setError("number", {
-          message: addMasterSemesterError.errors.number,
-        });
-      }
+    if (addMasterSemesterError) {
+      // Handle errors if needed
     }
-    if (addMasterSemesterError.message) {
-      form.setError("root", { message: addMasterSemesterError.message });
-    }
-  }, [addMasterSemesterError, form]);
+  }, [addMasterSemesterError]);
 
-  const onSubmit = async (values) => {
+  const handleToggle = (value) => {
+    setSelectedSemester(value);
+  };
+
+  const onSubmit = async () => {
     const isCompletedWithoutError = await dispatch(
       addMasterSemesters({
-        name: values.name,
-        number: values.number,
-        departmentId,
+        name : `Semester ${selectedSemester}`,
+        departmentId : departmentId,
+        number : selectedSemester
       })
     );
     if (isCompletedWithoutError) {
@@ -75,53 +46,27 @@ const AddMasterSemesterForm = ({ departmentId, handleClose }) => {
   };
 
   return (
-    <div>
-      <Form {...form}>
-        <form className="space-y-2" onSubmit={form.handleSubmit(onSubmit)}>
-          <FormRootError />
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter Semester name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <FormField
-            control={form.control}
-            name="number"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Number</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter Semester Number" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
+    <div>      
+          <ToggleGroup type="single" className="mb-6" value={selectedSemester} onChange={handleToggle}>
+            <ToggleGroupItem value="3" onClick={() => handleToggle("3")}>Semester 3</ToggleGroupItem>
+            <ToggleGroupItem value="4" onClick={() => handleToggle("4")}>Semester 4</ToggleGroupItem>
+            <ToggleGroupItem value="5" onClick={() => handleToggle("5")}>Semester 5</ToggleGroupItem>
+            <ToggleGroupItem value="6" onClick={() => handleToggle("6")}>Semester 6</ToggleGroupItem>
+          </ToggleGroup>
+
           <div className="h-1"></div>
           <DialogFooter>
             <Button
               isLoading={isCallingAddMasterSemesterApi}
               className="w-full"
               type="submit"
+              onClick = {onSubmit}
             >
               Submit
             </Button>
           </DialogFooter>
-        </form>
-      </Form>
     </div>
   );
 };
+
 export default AddMasterSemesterForm;
